9033bdc2477d73bacb188809762cda53265eda43
Helen <elena-kaminskaya@list,ru>
2023-05-29 15:29:39 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Human first = new Human("Иван", "Иванов");
+        System.out.println(first);
+    }
+}
